62ed71d9da6db65832f86a922ee7ea24641b96ed
Troy <troyswanson1010@gmail.com>
2021-10-10 20:16:53 -0500

    removed TMCommonKotlin

--- app/src/main/java/com/example/mvvmandretrofitplayground/view/FirstFragment.kt
+++ app/src/main/java/com/example/mvvmandretrofitplayground/view/FirstFragment.kt
@@ -11,8 +11,10 @@ import androidx.navigation.fragment.findNavController
 import com.example.mvvmandretrofitplayground.R
 import com.example.mvvmandretrofitplayground.databinding.FragmentFirstBinding
 import com.example.mvvmandretrofitplayground.presentation.FirstFragmentVM
-import com.tminus1010.tmcommonkotlin.rx.extensions.observe
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.plusAssign
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -25,6 +27,7 @@ class FirstFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    var disposables = CompositeDisposable() // If you use .observe(), then you do not need this
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,10 +40,19 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // # Bind Presentation Events
-        mainActivityVM.navToSecondFragment.observe(viewLifecycleOwner) { findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment) }
-        mainActivityVM.showToast.observe(viewLifecycleOwner) { Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show() }
+        mainActivityVM.navToSecondFragment
+            .observeOn(AndroidSchedulers.mainThread()) // If you use .observe(), then you do not need this
+            .subscribe { findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment) }
+            .also { disposables += it } // If you use .observe(), then you do not need this
+        mainActivityVM.showToast
+            .observeOn(AndroidSchedulers.mainThread()) // If you use .observe(), then you do not need this
+            .subscribe { Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show() }
+            .also { disposables += it } // If you use .observe(), then you do not need this
         // # Bind Presentation State
-        mainActivityVM.characterNames.observe(viewLifecycleOwner) { binding.textviewFirst.text = it }
+        mainActivityVM.characterNames
+            .observeOn(AndroidSchedulers.mainThread()) // If you use .observe(), then you do not need this
+            .subscribe { binding.textviewFirst.text = it }
+            .also { disposables += it } // If you use .observe(), then you do not need this
         // # Call UserIntents
         binding.buttonFirst.setOnClickListener { mainActivityVM.userTryNavForward() }
     }
@@ -48,5 +60,6 @@ class FirstFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        disposables.clear()
     }
 }
